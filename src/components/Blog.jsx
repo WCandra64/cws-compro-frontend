6ee4a1c6389a4e1c904232d433d5053a74156472
Wildan Mirtaza <wildanmirtaza@gmail.com>
2023-07-10 13:46:39 +0700

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,8 +8,8 @@ const Blog = () => {
     <div className='w-full pb-16 px-20 border-2' id='blog'>
         <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
             <div className='py-4'>
-              <h1 className='py-3 text-3xl font-bold'><span className='text-[#11aaff]'>Blog</span></h1>
-              <p className='text-[#6D737A]'>Temukan artikel terbaru kami.</p>
+              <h1 className='py-3 text-3xl font-bold'><span className='text-[#11aaff]'>Artikel</span></h1>
+              <p className='text-[#6D737A]'>Baca artikel terbaru kami.</p>
             </div>
             
             <div className='flex grid grid-cols-4'>
