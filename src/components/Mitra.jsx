06b45cd1019f650df577fd75e3fb5d9d6b5d219c
Wildan Mirtaza <wildanmirtaza@gmail.com>
2023-07-10 13:44:25 +0700

    Update Mitra.jsx

--- src/components/Mitra.jsx
+++ src/components/Mitra.jsx
@@ -6,7 +6,7 @@ const Mitra = () => {
     <div className='w-full bg-white py-20 bg-[#11aaff]/[.1] scroll-p-4' id='mitra'>
         <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
             <h1 className='text-center text-2xl font-bold text-[#325ccc]'>Mitra</h1>
-            <p className='text-center  text-[#325ccc] text-xl'>Mitra-mitra yang telah membantu kami.</p>
+            <p className='text-center  text-[#325ccc] text-xl'>Mitra-mitra yang telah bekerja sama dengan kami.</p>
             <div className='flex justify-center py-8 md:gap-8 '>
                 <div className='grid md:grid-cols-4 grid-cols-2 gap-2'>
                   <img src={companyLogo1} />
