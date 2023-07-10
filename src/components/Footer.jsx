d678c4b9181954bf3b322c07fb74c3612693fd45
Wildan Mirtaza <wildanmirtaza@gmail.com>
2023-07-10 14:53:38 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,13 @@ const Footer = () => {
                 {/* <img src={logo} className="h-[25px]" /> */}
                 <h2 className='font-[Exo] font-bold text-2xl tracking-[6px] text-[#325ccc]'>LOGO</h2>
                 <h3 className='text-2xl font-bold mt-10'>Contact Us</h3>
-                <h3 className='py-2 text-[#6D737A]'>Call : +123 400 123</h3>
-                <h3 className='py-2 text-[#6D737A]'>Praesent nulla massa, hendrerit <br></br> vestibulum gravida in, feugiat auctor felis.</h3>
-                <h3 className='py-2 text-[#363A3D]'>Email: example@mail.com</h3>
+                <h3 className='py-2 text-[#6D737A]'>Call : +6281234567890</h3>
+                <h3 className='py-2 text-[#6D737A]'>Jalan Hayam Wuruk 135<br></br>Denpasar</h3>
+                <h3 className='py-2 text-[#363A3D]'>Email: admin@cws.com</h3>
                 <div className='flex gap-4 py-4'>
                         <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaFacebookF size={25} style={{color:'#4DC39E'}} /></div>
-                        <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaDribbble size={25} style={{color:'#4DC39E'}} /></div>
                         <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaLinkedinIn size={25} style={{color:'#4DC39E'}} /></div>
                         <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaInstagram size={25} style={{color:'#4DC39E'}} /></div>
-                        <div className='p-4 bg-[#E9F8F3] rounded-xl'><FaBehance size={25} style={{color:'#4DC39E'}} /></div>
-
                 </div>
 
             </div>
@@ -28,12 +25,9 @@ const Footer = () => {
             <div>
                 <h3 className='text-2xl font-bold'>Explore</h3>
                 <ul className='py-6 text-[#6D737A]'>
-                    <li className='py-2'>Home</li>
-                    <li className='py-2'>About</li>
-                    <li className='py-2'>Course</li>
-                    <li className='py-2'>Blog</li>
-                    <li className='py-2'>Contact</li>
-
+                    <li className='py-2'>Beranda</li>
+                    <li className='py-2'>Artikel</li>
+                    <li className='py-2'>Mitra</li>
                 </ul>
             </div>
 
