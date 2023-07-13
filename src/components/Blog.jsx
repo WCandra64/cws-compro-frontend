8e183d35da9f84528d292f9273d03e98c56e20d4
WCandra64 <wcandra01@gmail.com>
2023-07-13 13:32:19 +0700

    Pindah Tombol Artikel Lainnya

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,15 +8,12 @@ const Blog = () => {
   return (
     <div className='w-full pb-16 px-20 border-2' id='blog'>
         <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
-          <div className='flex grid grid-cols-2 relative'>
+          {/* <div className='flex grid grid-cols-2 relative'> */}
             <div className='py-4'>
               <h1 className='py-3 text-3xl font-bold'><span className='text-[#11aaff]'>Artikel</span></h1>
               <p className='text-[#6D737A]'>Baca artikel terbaru kami.</p>
             </div>
-            <div className='absolute right-0 bottom-0 rounded-md bg-[#11aaff] hover:bg-[#325ccc] hover:drop-shadow-[1px_4px_4px_rgba(0,0,0,0.3)] hover:cursor-pointer py-2 px-6' onClick={() => window.location.href = "#"}>
-              <span className='text-center text-white font-medium '>Artikel Lainnya</span>
-            </div>
-          </div>
+          {/* </div> */}
             
             <div className='flex grid grid-cols-3'>
               {blog.slice(-3).toReversed().map((artikel, i) => (
@@ -33,23 +30,10 @@ const Blog = () => {
               </div> */}
             </div>
             
-
-            {/* {[...Array(blog)].map(
-              (artikel, i) => (
-                <div key={i}>
-                  <Card artikel={artikel} />
-                </div>
-              )
-            )} */}
-
-            {/* <Slider {...settings} className='px-5 z-2'>
-              {blog.map((artikel,i)=>
-                <div key={i}>
-                  <Card artikel={artikel} />
-                </div> ) }
-              
-
-            </Slider> */}
+            
+            <div className='w-fit rounded-md bg-[#11aaff] hover:bg-[#325ccc] hover:drop-shadow-[1px_4px_4px_rgba(0,0,0,0.3)] hover:cursor-pointer py-2 px-12 m-auto mt-4 text-center' onClick={() => window.location.href = "#"}>
+              <span className='text-white font-medium '>Artikel Lainnya</span>
+            </div>
             
         </div>
 
