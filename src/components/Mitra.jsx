dc4ca39eeaf839f602cbdfa34ae638ea6a18881e
WCandra64 <wcandra01@gmail.com>
2023-07-13 18:05:26 +0700

    Mitra Responsive Layout

--- src/components/Mitra.jsx
+++ src/components/Mitra.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { companyLogo1, companyLogo2, companyLogo3, companyLogo4  } from '../assets';
+import { mitra } from '../data/Mitra';
 
 const Mitra = () => {
   return (
-    <div className='w-full bg-white py-20 bg-[#11aaff]/[.1] scroll-p-4' id='mitra'>
+    <div className='w-full bg-white py-20 bg-[#cfe8ff] scroll-p-4' id='mitra'>
         <div className='md:max-w-[1480px] m-auto max-w-[600px]  px-4 md:px-0'>
             <h1 className='text-center text-2xl font-bold text-[#325ccc]'>Mitra</h1>
             <p className='text-center  text-[#325ccc] text-xl'>Mitra-mitra yang telah bekerja sama dengan kami.</p>
-            <div className='flex justify-center py-8 md:gap-8 '>
-                <div className='grid md:grid-cols-4 grid-cols-2 gap-2'>
-                  <img src={companyLogo1} />
-                  <img src={companyLogo2} />
-                  <img src={companyLogo3} />
-                  <img src={companyLogo4} />
-                </div>
+            <div className='flex justify-center py-8 gap-8 flex-wrap'>
+                {/* <div className='grid xl:grid-cols-7 grid-cols-4 gap-4 place-items-center'> */}
+                  {mitra.map((logo, i) => (
+                    <div key={i}>
+                      <img className='py-2' alt={logo.alt} src={logo.img} />
+                    </div>
+                  ))}
             </div>
         
         </div>
---
+++ src/data/Mitra.js
@@ -0,0 +1,62 @@
+export const mitra = [
+    {
+      id: 1,
+      alt: 'Company 1',
+      img: 'src/assets/company-logo-1.png'
+    },
+    {
+      id: 2,
+      alt: 'Company 2',
+      img: 'src/assets/company-logo-2.png',
+    },
+    {
+      id: 3,
+      alt: 'Company 3',
+      img: 'src/assets/company-logo-3.png',
+    },
+    {
+      id: 4,
+      alt: 'Company 4',
+      img: 'src/assets/company-logo-4.png',
+    },
+    {
+      id: 5,
+      alt: 'Company 3',
+      img: 'src/assets/company-logo-3.png',
+    },
+    {
+      id: 6,
+      alt: 'Company 2',
+      img: 'src/assets/company-logo-2.png',
+    },
+    {
+      id: 7,
+      alt: 'Company 1',
+      img: 'src/assets/company-logo-1.png',
+    },
+    {
+      id: 8,
+      alt: 'Company 2',
+      img: 'src/assets/company-logo-2.png',
+    },
+    {
+      id: 9,
+      alt: 'Company 3',
+      img: 'src/assets/company-logo-3.png',
+    },
+    {
+      id: 10,
+      alt: 'Company 2',
+      img: 'src/assets/company-logo-2.png',
+    },
+    {
+      id: 11,
+      alt: 'Company 1',
+      img: 'src/assets/company-logo-1.png',
+    },
+    {
+      id: 12,
+      alt: 'Company 2',
+      img: 'src/assets/company-logo-2.png',
+    },
+  ];
