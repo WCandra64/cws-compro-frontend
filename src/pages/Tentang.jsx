52f1f68ccac15dd78c0113614a538271b429a769
distiano <itodistiano22@gmail.com>
2023-07-18 14:40:02 +0800

    Update Tentang.jsx

--- src/pages/Tentang.jsx
+++ src/pages/Tentang.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 // import './App.css'
 
 const Tentang = () => {
   return (
-    <div className='py-[10vh]'>
-      <div className='pt-[8vh] pb-[15vh]'>
-        <h1 className='text-center font-bold text-2xl text-[#11aaff]'>Tentang</h1>
-        <h1 className='text-center font-bold text-xl'>PT. <span className='text-[#325ccc]'>Cahaya</span> <span className='text-[#11aaff]'>Wahana</span> <span className='text-[#fd3555]'>Sejahtera</span></h1>
+    <div className="py-[10vh]">
+      <div className="pt-[8vh] pb-[15vh]">
+        <h1 className="text-center font-bold text-2xl text-[#11aaff]">Tentang</h1>
+        <h1 className="text-center font-bold text-xl">
+          PT. <span className="text-[#325ccc]">Cahaya</span> <span className="text-[#11aaff]">Wahana</span> <span className="text-[#fd3555]">Sejahtera</span>
+        </h1>
       </div>
-    
-      <div className='px-20'>
+
+      <div className="px-20 text-center">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor tincidunt nunc non semper. In pharetra aliquet suscipit. Aenean a pretium orci, id sagittis lorem. In at luctus lorem. Suspendisse potenti. Vivamus enim nisi, vestibulum in justo venenatis, pretium rutrum eros. Nullam molestie iaculis odio, id laoreet tellus pulvinar sed.
-          <br /><br />
-          Nam auctor lorem id ante euismod, quis eleifend mi facilisis. Sed eleifend augue eu nisi sollicitudin elementum. Maecenas enim tellus, facilisis id quam eget, sodales pretium nulla. Phasellus sollicitudin, lorem ac efficitur facilisis, magna tortor tincidunt sapien, et maximus lorem dolor in dui. Quisque dignissim dictum libero, vel dignissim massa porttitor eu. In eu accumsan erat. Fusce imperdiet ligula vehicula sodales fringilla. Quisque non tortor massa. Sed semper, arcu quis tristique vehicula, leo sem sagittis lectus, at accumsan dui diam et nisl. Phasellus purus lorem, tristique sit amet finibus ac, varius vitae erat.
+          Kompetindo bergerak di bidang pelatihan dan sertifikasi, akademi, dan konsultan di bidang Teknologi Informasi dan Komunikasi, berdiri sejak tahun 2016 dan telah berbadan hukum sejak tahun 2019. Program yang kami rancang berfokus
+          pada peningkatan kemampuan & keterampilan seseorang (IT Training) dan validasi internasional (sertifikasi) sehingga diakui di seluruh dunia. <br />
+          <br />
+          Pada tahun 2020 saat pandemi melanda seluruh dunia, kami mengembangkan program kami menjadi pelatihan online yang memadukan antara pembelajaran sinkron dan asinkron. Saat ini, metode yang dilakukan sudah teruji dan sangat diminati
+          oleh mitra kami.
         </p>
       </div>
-        
     </div>
-  )
-}
+  );
+};
 
-export default Tentang
+export default Tentang;
